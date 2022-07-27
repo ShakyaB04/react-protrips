d453c76826e7a0b5616e369bac6b3be73a8702de
ShakyaB04 <shakyabhattacharyya@gmail.com>
2022-07-27 18:50:21 +0530

    by Shakya Bhattacharyya

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import AddTrip from "./Components/AddTrip";
+import List from "./Components/List";
+import Nav from "./Components/Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      trips: [],
+    };
+  }
+  
+  addTrip = (createNewTrip) => {
+    this.setState({
+      trips: [...createNewTrip],
+    });
+  };
+  
+  render() {
+    return (
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home trips={this.state.trips} />} />
+          <Route
+            path="/addTrip"
+            element={
+              <AddTrip addTrip={this.addTrip} trips={this.state.trips} />
+            }
+          />
+          <Route path="/list" element={<List trips={this.state.trips} />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
